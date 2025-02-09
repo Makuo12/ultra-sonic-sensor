6d38c857bd4649d8ddfd845f55f51f76192cba8a
makuo12 <uwau2544@gmail.com>
2025-02-09 16:27:33 +0100

    put everything in the right format

--- src/bin/async_main.rs
+++ src/bin/async_main.rs
@@ -2,7 +2,11 @@
 #![no_main]
 
 use esp_backtrace as _;
-use esp_hal::{clock::CpuClock, gpio::{Input, Level, Output}, rtc_cntl::Rtc};
+use esp_hal::{
+    clock::CpuClock,
+    gpio::{Input, Level, Output},
+    rtc_cntl::Rtc,
+};
 use esp_println::println;
 
 use embassy_executor::Spawner;
@@ -12,7 +16,6 @@ extern crate alloc;
 
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
-
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
 
@@ -23,7 +26,7 @@ async fn main(spawner: Spawner) {
     let timer0 = esp_hal::timer::systimer::SystemTimer::new(peripherals.SYSTIMER);
     esp_hal_embassy::init(timer0.alarm0);
     let mut trig = Output::new(peripherals.GPIO2, Level::Low);
-    let mut echo  = Input::new(peripherals.GPIO0, esp_hal::gpio::Pull::Up);
+    let mut echo = Input::new(peripherals.GPIO0, esp_hal::gpio::Pull::Up);
     let rtc = Rtc::new(peripherals.LPWR);
     let _ = spawner;
     loop {
@@ -31,11 +34,14 @@ async fn main(spawner: Spawner) {
         println!("Distance: {:.2} cm", distance);
         Timer::after(Duration::from_secs(1)).await;
     }
-
 }
 
-async fn get_soner(trig: &mut Output<'_>, echo: &mut Input<'_>,
-rtc: &Rtc<'_>, sound_velocity: f64) -> f64 {
+async fn get_soner(
+    trig: &mut Output<'_>,
+    echo: &mut Input<'_>,
+    rtc: &Rtc<'_>,
+    sound_velocity: f64,
+) -> f64 {
     trig.set_high();
     handle_delay(embassy_time::Delay, 10).await;
     trig.set_low();
@@ -44,9 +50,7 @@ rtc: &Rtc<'_>, sound_velocity: f64) -> f64 {
     echo.wait_for_low().await;
     let end = rtc.current_time();
     let result = end - start;
-    let distance = (result.num_microseconds().unwrap() as f64) * sound_velocity / 2.0 / 10_000.0;
-    return distance
-
+    (result.num_microseconds().unwrap() as f64) * sound_velocity / 2.0 / 10_000.0
 }
 
 async fn handle_delay<D: embedded_hal_async::delay::DelayNs>(mut delay: D, value: u32) {
